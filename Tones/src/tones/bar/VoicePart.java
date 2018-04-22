0a26fd9a7196c598d72d76efd0f6e1418beea71b
David Wright <24604124+dimwight@users.noreply.github.com>
2018-04-22 20:11:42 +0100

    Update VoicePart.java

--- Tones/src/tones/bar/VoicePart.java
+++ Tones/src/tones/bar/VoicePart.java
@@ -112,9 +112,7 @@ public final class VoicePart extends Tracer{
 	
 	public VoicePart(String src){
 		this.src=src;
-		voice=parseSource(src,barTones,barCodes);
-	}
-	private static Voice parseSource(String src,List<List<Tone>>barTones){?
+		
 		String splitVoice[]=src.split(":",2),
 			voiceCode=splitVoice[0].substring(0).toLowerCase();
 		Voice voice=voiceCode.equals("e")?Empty:
