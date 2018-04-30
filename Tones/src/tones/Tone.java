5b2434c0eba9af0317fc4b7dcb71bef1b0c2a943
David Wright <24604124+dimwight@users.noreply.github.com>
2018-04-30 11:25:46 +0000

    Update Tone.java

--- Tones/src/tones/Tone.java
+++ Tones/src/tones/Tone.java
@@ -9,54 +9,66 @@ import java.util.Arrays;
 import java.util.HashSet;
 import tones.Mark.Tie;
 public final class Tone extends Tracer{
-	public static final short NOTE_WHOLE=8,NOTE_HALF=NOTE_WHOLE/2,
-		NOTE_QUARTER=NOTE_WHOLE/4,NOTE_EIGHTH=NOTE_WHOLE/8,
-		NOTE_DOUBLE=NOTE_WHOLE*2,NOTE_NONE=0;
-	public final int barAt,eighthAt;
-	public final Voice voice;
-	public final byte pitch;
-	public final short eighths;
-	public final HashSet<Mark>marks=new HashSet();
-	private final int[]intValues;
-	public Tone(Voice voice,int barAt,int eighthAt,byte pitch,short eighths){
-		this.voice=voice;
-		this.barAt=barAt;
-		this.eighthAt=eighthAt;
-		this.pitch=pitch;
-		this.eighths=eighths;
-		intValues=new int[]{barAt,eighthAt,pitch,eighths};
-	}
-	public DataNode newDebugNode(){
-		int markCount=marks.size();
-		return newDebugRoot(Tone.class,toString(),markCount==0?"No marks"
-				:newDebugRoot(Mark.class,"marks="+markCount,
-						Objects.toLines(marks.toArray()).split("\n")));
-	}
-	public void checkTied(Tone before){
-		if(before==null||before.isRest()||isRest()
-				||before.pitch!=pitch||!isOnBeat(Tone.NOTE_HALF))return;
-			Tie tie=new Tie(before,this);
-			marks.add(tie);
-			before.marks.add(tie);
-	}
-	public boolean isOnBeat(short note){
-		return eighthAt%note==0;
-	}
-	private boolean isRest(){
-		return this.pitch==PITCH_REST;
-	}
-	public String toString(){
-		ScaleNote note=pitchNote();
-		return voice+" "+pitchNote()+": "+Strings.intsString(intValues);
-	}
-	public ScaleNote pitchNote(){
-		return ScaleNote.pitchNote(pitch);
-	}
-	public int hashCode(){
-		return Arrays.hashCode(intValues);
-	}
-	public boolean equals(Object o){
-		Tone that=(Tone)o;
-		return this==that||(voice==that.voice&&Arrays.equals(intValues,that.intValues));
-	}
+  public static final short NOTE_WHOLE=8,NOTE_HALF=NOTE_WHOLE/2,
+    NOTE_QUARTER=NOTE_WHOLE/4,NOTE_EIGHTH=NOTE_WHOLE/8,
+    NOTE_DOUBLE=NOTE_WHOLE*2,NOTE_NONE=0;
+  public final int barAt,eighthAt;
+  public final Voice voice;
+  public final byte pitch;
+  public final short eighths;
+  public final HashSet<Mark>marks=new HashSet();
+  private final int[]intValues;
+  private boolean isOffset;
+  public Tone(Voice voice,int barAt,int eighthAt,byte pitch,short eighths){
+    this.voice=voice;
+    this.barAt=barAt;
+    this.eighthAt=eighthAt;
+    this.pitch=pitch;
+    this.eighths=eighths;
+    intValues=new int[]{barAt,eighthAt,pitch,eighths};
+  }
+  public DataNode newDebugNode(){
+    int markCount=marks.size();
+    return newDebugRoot(Tone.class,toString(),markCount==0?"No marks"
+        :newDebugRoot(Mark.class,"marks="+markCount,
+            Objects.toLines(marks.toArray()).split("\n")));
+  }
+  public void checkTied(Tone before){
+    if(before==null||before.isRest()||isRest()
+        ||before.pitch!=pitch||!isOnBeat(Tone.NOTE_HALF))return;
+      Tie tie=new Tie(before,this);
+      marks.add(tie);
+      before.marks.add(tie);
+  }
+  public boolean isOnBeat(short note){
+    return eighthAt%note==0;
+  }
+  private boolean isRest(){
+    return this.pitch==PITCH_REST;
+  }
+  public String toString(){
+    ScaleNote note=pitchNote();
+    return voice+" "+pitchNote()+": "+Strings.intsString(intValues);
+  }
+  public ScaleNote pitchNote(){
+    return ScaleNote.pitchNote(pitch);
+  }
+  public int hashCode(){
+    return Arrays.hashCode(intValues);
+  }
+  public boolean equals(Object o){
+    Tone that=(Tone)o;
+    return this==that||(voice==that.voice&&Arrays.equals(intValues,that.intValues));
+  }
+  public void checkOffset(Incipit i){
+    for(Tone that:i.tones)
+      if(Math.abs(that.pitch-pitch)<2)
+        isOffset=that.eighths>eighths
+          ||(that.eighths=eighths&&!that.marks.isEmpty())
+          ||!that.isOffset();
+  }
+  public boolean isOffset(){
+    return isOffset;
+  }
+  
 }
