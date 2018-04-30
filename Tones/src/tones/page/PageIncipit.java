042ff5d93f47f3ece266bb4dcfa95a30049733e7
David Wright <24604124+dimwight@users.noreply.github.com>
2018-04-30 11:30:16 +0000

    Update PageIncipit.java

--- Tones/src/tones/page/PageIncipit.java
+++ Tones/src/tones/page/PageIncipit.java
@@ -5,19 +5,15 @@ import tones.Tone;
 import tones.bar.Bar;
 import tones.bar.Incipit;
 final class PageIncipit extends PageItem{
-	public final Incipit content;
-	private final double scaledPageX;
-	PageIncipit(Incipit content,double barPageX,double pageXScale){
-		this.content=content;
-		scaledPageX=(int)(barPageX+content.barAt*pageXScale);
-	}
-	double tonePageX(Tone tone){
-		if(scaledPageX<0)throw new IllegalStateException(
-				"Invalid scaledPageX in "+Debug.info(this));
-		double scaled=scaledPageX;
-		for(Tone that:content.tones)
-			if(Math.abs(that.pitch-tone.pitch)<2
-					&&(tone.eighths>=that.eighths))scaled+=Bar.WIDTH_NOTE;
-		return scaled;
-	}	
+  public final Incipit content;
+  private final double scaledPageX;
+  PageIncipit(Incipit content,double barPageX,double pageXScale){
+    this.content=content;
+    scaledPageX=(int)(barPageX+content.barAt*pageXScale);
+  }
+  double tonePageX(Tone tone){
+    if(scaledPageX<0)throw new IllegalStateException(
+        "Invalid scaledPageX in "+Debug.info(this));
+    else return scaledPageX+(tone.isOffset()?Bar.WIDTH_NOTE:0)
+  } 
 }
