6b3fe6851285de6116ceba04444ef1982ec49ffb
David Wright <24604124+dimwight@users.noreply.github.com>
2018-04-30 11:36:29 +0000

    Update PageNote.java

--- Tones/src/tones/page/PageNote.java
+++ Tones/src/tones/page/PageNote.java
@@ -9,64 +9,63 @@ import tones.Clef;
 import tones.Tone;
 import tones.bar.Incipit;
 public class PageNote extends PageItem{
-	public enum Dot{NONE,LEVEL,BELOW,ABOVE};
-	public final Tone tone;
-	public final double pageX,pageY,ledgerLineShift;
-	public final int ledgerLines;
-	public boolean selected;
-	public final Line tail,tailSwag;
-	public final Vector at;
-	public final Dot dotAt;
-	final PageBar bar;
-	private final String debugString;
-	PageNote(PageBar bar,Tone tone,PageIncipit i,double barPageY,Clef clef, 
-			boolean selected){
-		this.bar=bar;
-		this.tone=tone;
-		this.selected=selected;
-		pageX=i.tonePageX(tone);
-		final int stavePitch=tone.pitch-clef.staveMidPitch,
-			staveToMidPitch=STAVE_GRID/2-1;
-		pageY=barPageY+staveToMidPitch-stavePitch;
-		at=new Vector(pageX,pageY);
-		boolean aboveMidPitch=stavePitch>0;
-		int beyondStave=Math.abs(stavePitch)-staveToMidPitch;
-		ledgerLines=beyondStave<=0?0
-				:beyondStave/2*(aboveMidPitch?1:-1);
-		ledgerLineShift=aboveMidPitch?beyondStave%2+1:(beyondStave+1)%2;
-		boolean tailsUp=tone.voice.tailsUp;
-		dotAt=tone.eighths%3!=0?NONE:stavePitch%2==0&&ledgerLines==0?ABOVE:LEVEL;
-		debugString=tone.pitchNote()+
-			" stavePitch="+stavePitch+" aboveMidPitch="+aboveMidPitch
-			+" beyondStave="+beyondStave;
-		double tailHeight=tone.eighths>NOTE_QUARTER?4.7:5;
-		Point tailFrom=new Point(tailFrom(tone.eighths,tailsUp
-				).at().scaled(scaleToNoteWidth)),
-			tailTo=tailFrom.shifted(new Vector(0,tailHeight*(tailsUp?-1:1)));
-		tail=tone.eighths>6?null:new Line(tailFrom,tailTo);
-		tailSwag=tone.eighths>NOTE_EIGHTH?null
-				:new Line(tailTo,tailTo.shifted(new Vector(3,5)));
-	}
-	public Point tailFrom(short note,boolean tailsUp){
-		return tailsUp?
-				note>NOTE_QUARTER?new Point(0.9,-0.5):new Point(0.82,-0.2)
-				:note>NOTE_QUARTER?new Point(0.12,0.3):new Point(0.15,0.1);
-	}
-	public String toString(){
-		return false?debugString:tone.toString()+" "+staveAt();
-	}
-	public int hashCode(){
-		return Arrays.hashCode(intValues());
-	}
-	public boolean equals(Object obj){
-		if(true)throw new RuntimeException("Untested");
-		PageNote that=(PageNote)obj;
-		return this==that||Arrays.equals(intValues(),that.intValues());
-	}
-	private int[]intValues(){
-		return new int[]{tone.pitch,tone.eighths};
-	}
-	public Vector staveAt(){
-		return new Vector(pageX,pageY);
-	}
+  public enum Dot{NONE,LEVEL,BELOW,ABOVE};
+  public final Tone tone;
+  public final double pageX,pageY,ledgerLineShift;
+  public final int ledgerLines;
+  public boolean selected;
+  public final Line tail,tailSwag;
+  public final Vector at;
+  public final Dot dotAt;
+  final PageBar bar;
+  private final String debugString;
+  private final int[]intValues;
+  PageNote(PageBar bar,Tone tone,PageIncipit i,double barPageY,Clef clef, 
+      boolean selected){
+    this.bar=bar;
+    this.tone=tone;
+    intValues=new int[]{tone.pitch,tone.eighths};
+    this.selected=selected;
+    pageX=i.tonePageX(tone);
+    final int stavePitch=tone.pitch-clef.staveMidPitch,
+      staveToMidPitch=STAVE_GRID/2-1;
+    pageY=barPageY+staveToMidPitch-stavePitch;
+    at=new Vector(pageX,pageY);
+    boolean aboveMidPitch=stavePitch>0;
+    int beyondStave=Math.abs(stavePitch)-staveToMidPitch;
+    ledgerLines=beyondStave<=0?0
+        :beyondStave/2*(aboveMidPitch?1:-1);
+    ledgerLineShift=aboveMidPitch?beyondStave%2+1:(beyondStave+1)%2;
+    boolean tailsUp=tone.voice.tailsUp;
+    dotAt=tone.eighths%3!=0?NONE:stavePitch%2==0&&ledgerLines==0?ABOVE:LEVEL;
+    debugString=tone.pitchNote()+
+      " stavePitch="+stavePitch+" aboveMidPitch="+aboveMidPitch
+      +" beyondStave="+beyondStave;
+    double tailHeight=tone.eighths>NOTE_QUARTER?4.7:5;
+    Point tailFrom=new Point(tailFrom(tone.eighths,tailsUp
+        ).at().scaled(scaleToNoteWidth)),
+      tailTo=tailFrom.shifted(new Vector(0,tailHeight*(tailsUp?-1:1)));
+    tail=tone.eighths>6?null:new Line(tailFrom,tailTo);
+    tailSwag=tone.eighths>NOTE_EIGHTH?null
+        :new Line(tailTo,tailTo.shifted(new Vector(3,5)));
+  }
+  public Point tailFrom(short note,boolean tailsUp){
+    return tailsUp?
+        note>NOTE_QUARTER?new Point(0.9,-0.5):new Point(0.82,-0.2)
+        :note>NOTE_QUARTER?new Point(0.12,0.3):new Point(0.15,0.1);
+  }
+  public String toString(){
+    return false?debugString:tone.toString()+" "+staveAt();
+  }
+  public int hashCode(){
+    return Arrays.hashCode(intValues);
+  }
+  public boolean equals(Object obj){
+    if(true)throw new RuntimeException("Untested");
+    PageNote that=(PageNote)obj;
+    return this==that||Arrays.equals(intValues,that.intValues);
+  }
+  public Vector staveAt(){
+    return new Vector(pageX,pageY);
+  }
 }
