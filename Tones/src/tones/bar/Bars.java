ea17621bb008f26b0ed1567cf7ee4c71f705a20f
David Wright <24604124+dimwight@users.noreply.github.com>
2018-04-30 12:18:26 +0000

    Update Bars.java

--- Tones/src/tones/bar/Bars.java
+++ Tones/src/tones/bar/Bars.java
@@ -17,131 +17,131 @@ import tones.Tone;
 import tones.Voice;
 import tones.app.TonesViewable;
 public final class Bars extends Tracer implements Titled{
-	public static final boolean eighthsCheck=false;
-	private final List<Bar>bars=new ArrayList();
-	private final Map<Voice,VoicePart>parts=new HashMap();
-	private final TonesViewable viewable;
-	private VoicePart selectedPart;
-	private int barEighths;
-	public Bars(TonesViewable viewable){
-		this.viewable=viewable;
-		TypedNode[]children=viewable.contentTree().children();
-		for(TypedNode child:children){
-			VoicePart part=new VoicePart((String)child.values()[0]);
-			parts.put(part.voice,part);
-			if(child==viewable.selection().single())
-				selectPart(part.voice);
-		}
-		if(selectedPart==null)selectPart(Empty);
-		int barAt=0;
-		barEighths=0;
-		while(true){
-			Bar bar=newPartsBar(barAt++);
-			if(bar!=null)bars.add(bar);
-			else break;
-		}
-		if(false)trace(": bars="+bars.size()+" barAt="+barAt);
-	}
-	private Bar newPartsBar(int barAt){
-		Map<Integer,Incipit>incipits=new HashMap();
-		for(VoicePart part:parts.values()){
-			List<Tone>tones=part.getBarTones(barAt);
-			if(false&&part.voice==Bass)trace(".newVoiceLinesBar: tones="+tones.size()+" barAt="+barAt);
-			int barEighthsNow=tones.isEmpty()?barEighths
-					:(eighthsCheck?tones.remove(0):tones.get(0)).eighths;
-			if(tones.isEmpty())continue;
-			if(eighthsCheck&&barEighths!=0&&barEighthsNow!=barEighths)throw new IllegalStateException(
-					"New barEighths="+barEighths+", barEighthsNow="+barEighthsNow+
-					" in "+Debug.info(part));
-			else barEighths=barEighthsNow;
-			int eighthAt=0;
-			for(Tone tone:tones){
-				Incipit i;
-				if((i=incipits.get(eighthAt))==null)
-					incipits.put(eighthAt,i=new Incipit(eighthAt));
-				i.addTone(tone);
-				eighthAt+=tone.eighths;
-			}
-		}
-		return incipits.isEmpty()?null:new Bar(barAt++,incipits.values(),barEighths);
-	}
-	public void updatePart(String src){
-		VoicePart nowPart=new VoicePart(src),
-			thenPart=parts.replace(nowPart.voice,nowPart);
-		selectPart(nowPart.voice);
-		int count=bars.size();
-		for(int start=true?0:count-5,stop=false?2:count,
-				barAt=start;true;barAt++){
-			List<Tone>thenTones=thenPart.getBarTones(barAt),
-					nowTones=nowPart.getBarTones(barAt);
-			boolean nowEmpty=nowTones.isEmpty();
-			if(nowEmpty&&thenTones.isEmpty())break;			
-			barEighths=eighthsCheck&&!nowEmpty?nowTones.remove(0).eighths
-					:VoicePart.BAR_EIGHTHS_DEFAULT;
-			boolean equals=thenTones.equals(nowTones);
-			if(!equals) {
-				if(false) {
-					trace(".updateSelectedPart: barAt="+barAt
-							+ " equals="+equals+" now=",nowTones);
-					trace(" then=",thenTones);
-				}
-				if(barAt<bars.size())bars.remove(barAt);
-				bars.add(barAt,newPartsBar(barAt));
-			}
-		}
-		int stop=bars.size();
-		for(int at=0;at<stop;at++)if(bars.get(at)==null){
-			bars.remove(at--);
-			stop--;
-		}
-		if(false)trace(".updatePart: bars=",bars.size());
-	}
-	public void selectPart(Voice voice){
-		selectedPart=parts.get(voice);
-		for(TypedNode child:viewable.contentTree().children())
-			if(new VoicePart((String)child.values()[0]).voice.equals(selectedPart.voice))
-					viewable.defineSelection(child);
-	}
-	public VoicePart selectedPart(){
-		return selectedPart;
-	}
-	public int barCount(){
-		return bars.size();
-	}
-	public List<Bar>barsFrom(int at){
-		return bars.subList(at,bars.size());
-	}
-	public String title(){
-		return viewable.title();
-	}
-	public DataNode newDebugRoot(int start,int stop){
-		if(false)trace(".newDebugRoot: start="+start+" stop="+stop);
-		NodeList barsList=new NodeList(newDebugRoot(Bars.class,title()),true);
-		for(Bar bar:barsFrom(start)){
-			if(bar.at==stop)break;
-			NodeList barList=new NodeList(newDebugRoot(Bar.class,""+bar.at),true);
-			barsList.add(barList.parent);
-			List<Incipit>incipits=new ArrayList<Incipit>(bar.incipits);
-			Collections.sort(incipits);
-			for(Incipit incipit:incipits){
-				String title="at="+incipit.eighthAt;
-				NodeList incipitList=new NodeList(newDebugRoot(Incipit.class,title),true);
-				barList.add(incipitList.parent);
-				List<Tone>sortTones=new ArrayList(incipit.tones);
-				Collections.sort(sortTones,new Comparator<Tone>(){
-					@Override
-					public int compare(Tone t1,Tone t2){
-						return t1.voice.compareTo(t2.voice);
-					}
-				});
-				for(Tone tone:sortTones){
-					incipitList.add(tone.newDebugNode());
-				}
-			}
-		}
-		return barsList.parent;
-	}
-	static public DataNode newDebugRoot(Class type,String title,Object...values){
-		return new DataNode(type.getSimpleName(),title,values);
-	}
+  public static final boolean eighthsCheck=false;
+  private final List<Bar>bars=new ArrayList();
+  private final Map<Voice,VoicePart>parts=new HashMap();
+  private final TonesViewable viewable;
+  private VoicePart selectedPart;
+  private int barEighths;
+  public Bars(TonesViewable viewable){
+    this.viewable=viewable;
+    TypedNode[]children=viewable.contentTree().children();
+    for(TypedNode child:children){
+      VoicePart part=new VoicePart((String)child.values()[0]);
+      parts.put(part.voice,part);
+      if(child==viewable.selection().single())
+        selectPart(part.voice);
+    }
+    if(selectedPart==null)selectPart(Empty);
+    int barAt=0;
+    barEighths=0;
+    while(true){
+      Bar bar=newPartsBar(barAt++);
+      if(bar!=null)bars.add(bar);
+      else break;
+    }
+    if(false)trace(": bars="+bars.size()+" barAt="+barAt);
+  }
+  private Bar newPartsBar(int barAt){
+    Map<Integer,Incipit>incipits=new HashMap();
+    for(VoicePart part:parts.values()){
+      List<Tone>partTones=part.getBarTones(barAt);
+      if(false&&part.voice==Bass)trace(".newVoiceLinesBar: partTones="+partTones.size()+" barAt="+barAt);
+      int barEighthsNow=partTones.isEmpty()?barEighths
+          :(eighthsCheck?partTones.remove(0):partTones.get(0)).eighths;
+      if(partTones.isEmpty())continue;
+      if(eighthsCheck&&barEighths!=0&&barEighthsNow!=barEighths)throw new IllegalStateException(
+          "New barEighths="+barEighths+", barEighthsNow="+barEighthsNow+
+          " in "+Debug.info(part));
+      else barEighths=barEighthsNow;
+      int partAt=0;
+      for(Tone tone:partTones){
+        Incipit i;
+        if((i=incipits.get(partAt))==null)
+          incipits.put(partAt,i=new Incipit(partAt));
+        i.addTone(tone);
+        partAt+=tone.eighths;
+      }
+    }
+    return incipits.isEmpty()?null:new Bar(barAt++,incipits.values(),barEighths);
+  }
+  public void updatePart(String src){
+    VoicePart nowPart=new VoicePart(src),
+      thenPart=parts.replace(nowPart.voice,nowPart);
+    selectPart(nowPart.voice);
+    int count=bars.size();
+    for(int start=true?0:count-5,stop=false?2:count,
+        barAt=start;true;barAt++){
+      List<Tone>thenTones=thenPart.getBarTones(barAt),
+          nowTones=nowPart.getBarTones(barAt);
+      boolean nowEmpty=nowTones.isEmpty();
+      if(nowEmpty&&thenTones.isEmpty())break;     
+      barEighths=eighthsCheck&&!nowEmpty?nowTones.remove(0).eighths
+          :VoicePart.BAR_EIGHTHS_DEFAULT;
+      boolean equals=thenTones.equals(nowTones);
+      if(!equals) {
+        if(false) {
+          trace(".updateSelectedPart: barAt="+barAt
+              + " equals="+equals+" now=",nowTones);
+          trace(" then=",thenTones);
+        }
+        if(barAt<bars.size())bars.remove(barAt);
+        bars.add(barAt,newPartsBar(barAt));
+      }
+    }
+    int stop=bars.size();
+    for(int at=0;at<stop;at++)if(bars.get(at)==null){
+      bars.remove(at--);
+      stop--;
+    }
+    if(false)trace(".updatePart: bars=",bars.size());
+  }
+  public void selectPart(Voice voice){
+    selectedPart=parts.get(voice);
+    for(TypedNode child:viewable.contentTree().children())
+      if(new VoicePart((String)child.values()[0]).voice.equals(selectedPart.voice))
+          viewable.defineSelection(child);
+  }
+  public VoicePart selectedPart(){
+    return selectedPart;
+  }
+  public int barCount(){
+    return bars.size();
+  }
+  public List<Bar>barsFrom(int at){
+    return bars.subList(at,bars.size());
+  }
+  public String title(){
+    return viewable.title();
+  }
+  public DataNode newDebugRoot(int start,int stop){
+    if(false)trace(".newDebugRoot: start="+start+" stop="+stop);
+    NodeList barsList=new NodeList(newDebugRoot(Bars.class,title()),true);
+    for(Bar bar:barsFrom(start)){
+      if(bar.at==stop)break;
+      NodeList barList=new NodeList(newDebugRoot(Bar.class,""+bar.at),true);
+      barsList.add(barList.parent);
+      List<Incipit>incipits=new ArrayList<Incipit>(bar.incipits);
+      Collections.sort(incipits);
+      for(Incipit incipit:incipits){
+        String title="at="+incipit.eighthAt;
+        NodeList incipitList=new NodeList(newDebugRoot(Incipit.class,title),true);
+        barList.add(incipitList.parent);
+        List<Tone>sortTones=new ArrayList(incipit.tones);
+        Collections.sort(sortTones,new Comparator<Tone>(){
+          @Override
+          public int compare(Tone t1,Tone t2){
+            return t1.voice.compareTo(t2.voice);
+          }
+        });
+        for(Tone tone:sortTones){
+          incipitList.add(tone.newDebugNode());
+        }
+      }
+    }
+    return barsList.parent;
+  }
+  static public DataNode newDebugRoot(Class type,String title,Object...values){
+    return new DataNode(type.getSimpleName(),title,values);
+  }
 }
