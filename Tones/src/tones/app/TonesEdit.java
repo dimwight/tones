7859d8b53a0b8c4461d30aa6e13bb5cbbf2bf59e
David Wright <24604124+dimwight@users.noreply.github.com>
2018-05-02 10:40:22 +0000

    Slider for local

--- Tones/src/tones/app/TonesEdit.java
+++ Tones/src/tones/app/TonesEdit.java
@@ -30,92 +30,92 @@ import applicable.treetext.TreeTextViewable;
 import tones.bar.VoicePart;
 import tones.view.PageView;
 public final class TonesEdit extends TreeTextContenter{
-	public static void main(String[]args){
-		newSpecifier().buildAndLaunchApp(args);
-	}
-	public static final String ARG_BAR_FROM="barFrom",ARG_RESCALE="rescale";
-	public TonesEdit(Object source,FacetAppSurface app){
-		super(source,app);
-	}
-	@Override
-	protected TreeTextViewable newViewable(DataNode tree){
-		return new TonesViewable(tree,app.ff.statefulClipperSource(false),app);
-	}
-	@Override
-	protected SFrameTarget[]newViewTargets(TreeView debugTree,boolean liveViews){
-		TonesViewable viewable=(TonesViewable)this.contentFrame();
-		int barFrom=app.spec.state().getOrPutInt(TonesEdit.ARG_BAR_FROM,1);
-		if(viewable.bars.barCount()<barFrom)barFrom=1;
-		SFrameTarget page=PageView.newFramed(8,app.spec,
-				viewable.bars.barCount(),barFrom);
-		return new SFrameTarget[]{page,
-				viewable.barsView,
-				new SFrameTarget(debugTree)};
-	}
-	@Override
-	protected void attachContentAreaFacets(AreaRoot area){
-		final FacetFactory ff=app.ff;
-		ViewerAreaMaster vam=new ViewerAreaMaster(){
-			protected ViewerAreaMaster newChildMaster(SAreaTarget child){
-				return !(((SViewer)child.activeFaceted()).view()instanceof PageView)?null
-						:new ViewerAreaMaster(){
-					protected SFacet newViewTools(STargeter viewTargeter){
-						STargeter barStart=viewTargeter.elements()[PageView.TARGET_BAR];
-						return ff.toolGroups(viewTargeter,HINT_PANEL_CENTER,ff.spacerWide(8),
-								false?null:ff.spacerTall(45),
-					  		ff.numericSliders(barStart,
-					  				350,HINT_SLIDER_TICKS+HINT_SLIDER_LABELS+HINT_SLIDER_LOCAL),
-					  		ff.numericNudgeButtons(barStart,
-					  				HINT_NUMERIC_FIELDS+HINT_NUMERIC_NUDGERS_FIRST));
-					}
-					protected String hintString(){
-						return HINT_NO_FLASH+HINT_PANEL_BORDER+HINT_BARE;
-					}
-				};
-			}
-		};
-		ff.areas().attachViewerAreaPanes(area,vam,AreaFacets.PANE_SPLIT_HORIZONTAL);
-	}
-	@Override
-	protected TreeTextFeatures newFeatures(SContentAreaTargeter area){
-		final STargeter selection=area.selection(),
-				code=selection.elements()[0];
-		return new TreeTextFeatures(app,area){
-			@Override
-			protected SFacet[]adjustToolbarItems(ItemList<SFacet>facets){
-				if(false)facets.clear();
-				facets.add(0,rowPanel(selection,textualField(code,50,HINT_NONE)));
-				return facets.items();
-			}
-		};
-	}
-	@Override
-	public void wasAdded(){
-		STarget[]viewers=app.activeContentTargeter().areaTarget().indexableTargets();
-		if(viewers.length==1)return;
-		SAreaTarget debug=(SAreaTarget)viewers[1];
-		SIndexing expand=(SIndexing)((Targeted)debug.activeFaceted().attachedFacet()
-				).targets()[0];
-		expand.setIndex(2);
-	}
-	private static TreeTextSpecifier newSpecifier(){
-		return new TreeTextSpecifier(TonesEdit.class){
-			protected XmlSpecifier[]fileSpecifiers(XmlPolicy policy){
-				return new XmlSpecifier[]{
-					new XmlSpecifier("tones.xml","Tones",policy),
-				};
-			}
-			@Override
-			protected Object getInternalContentSource(){
-				File file=new File(Util.runDir(),"E major.tones.xml");
-				return file.exists()?file 
-						:new ValueNode("xml","Tones"+contents++,new Object[]{
-								new ValueNode("Tones",VoicePart.TEST_CODES)}).copyState();
-			}
-			@Override
-			protected TreeTextContenter newContenter(Object source,FacetAppSurface app){
-				return new TonesEdit(source,app);
-			}
-		};
-	}
+  public static void main(String[]args){
+    newSpecifier().buildAndLaunchApp(args);
+  }
+  public static final String ARG_BAR_FROM="barFrom",ARG_RESCALE="rescale";
+  public TonesEdit(Object source,FacetAppSurface app){
+    super(source,app);
+  }
+  @Override
+  protected TreeTextViewable newViewable(DataNode tree){
+    return new TonesViewable(tree,app.ff.statefulClipperSource(false),app);
+  }
+  @Override
+  protected SFrameTarget[]newViewTargets(TreeView debugTree,boolean liveViews){
+    TonesViewable viewable=(TonesViewable)this.contentFrame();
+    int barFrom=app.spec.state().getOrPutInt(TonesEdit.ARG_BAR_FROM,1);
+    if(viewable.bars.barCount()<barFrom)barFrom=1;
+    SFrameTarget page=PageView.newFramed(8,app.spec,
+        viewable.bars.barCount(),barFrom);
+    return new SFrameTarget[]{page,
+        viewable.barsView,
+        new SFrameTarget(debugTree)};
+  }
+  @Override
+  protected void attachContentAreaFacets(AreaRoot area){
+    final FacetFactory ff=app.ff;
+    ViewerAreaMaster vam=new ViewerAreaMaster(){
+      protected ViewerAreaMaster newChildMaster(SAreaTarget child){
+        return !(((SViewer)child.activeFaceted()).view()instanceof PageView)?null
+            :new ViewerAreaMaster(){
+          protected SFacet newViewTools(STargeter viewTargeter){
+            STargeter barStart=viewTargeter.elements()[PageView.TARGET_BAR];
+            return ff.toolGroups(viewTargeter,HINT_PANEL_CENTER,ff.spacerWide(8),
+                false?null:ff.spacerTall(45),
+                ff.numericSliders(barStart,200,
+                    HINT_SLIDER_TICKS+HINT_SLIDER_LABELS+HINT_SLIDER_LOCAL),
+                ff.numericNudgeButtons(barStart,
+                    HINT_NUMERIC_FIELDS+HINT_NUMERIC_NUDGERS_FIRST));
+          }
+          protected String hintString(){
+            return HINT_NO_FLASH+HINT_PANEL_BORDER+HINT_BARE;
+          }
+        };
+      }
+    };
+    ff.areas().attachViewerAreaPanes(area,vam,AreaFacets.PANE_SPLIT_HORIZONTAL);
+  }
+  @Override
+  protected TreeTextFeatures newFeatures(SContentAreaTargeter area){
+    final STargeter selection=area.selection(),
+        code=selection.elements()[0];
+    return new TreeTextFeatures(app,area){
+      @Override
+      protected SFacet[]adjustToolbarItems(ItemList<SFacet>facets){
+        if(false)facets.clear();
+        facets.add(0,rowPanel(selection,textualField(code,50,HINT_NONE)));
+        return facets.items();
+      }
+    };
+  }
+  @Override
+  public void wasAdded(){
+    STarget[]viewers=app.activeContentTargeter().areaTarget().indexableTargets();
+    if(viewers.length==1)return;
+    SAreaTarget debug=(SAreaTarget)viewers[1];
+    SIndexing expand=(SIndexing)((Targeted)debug.activeFaceted().attachedFacet()
+        ).targets()[0];
+    expand.setIndex(2);
+  }
+  private static TreeTextSpecifier newSpecifier(){
+    return new TreeTextSpecifier(TonesEdit.class){
+      protected XmlSpecifier[]fileSpecifiers(XmlPolicy policy){
+        return new XmlSpecifier[]{
+          new XmlSpecifier("tones.xml","Tones",policy),
+        };
+      }
+      @Override
+      protected Object getInternalContentSource(){
+        File file=new File(Util.runDir(),"E major.tones.xml");
+        return file.exists()?file 
+            :new ValueNode("xml","Tones"+contents++,new Object[]{
+                new ValueNode("Tones",VoicePart.TEST_CODES)}).copyState();
+      }
+      @Override
+      protected TreeTextContenter newContenter(Object source,FacetAppSurface app){
+        return new TonesEdit(source,app);
+      }
+    };
+  }
 }
