9eb0112163320acc7fcdacabc1f77c82a8d50afb
David Wright <24604124+dimwight@users.noreply.github.com>
2018-05-05 13:41:58 +0100

    Update Incipit.java

--- Tones/src/tones/bar/Incipit.java
+++ Tones/src/tones/bar/Incipit.java
@@ -23,6 +23,9 @@ public final class Incipit extends Tracer implements Comparable<Incipit>{
     staveGap=10;
     fall=6;
   }
+  readSoundings(Soundings then){
+    
+  }
   public String toString(){
     return Debug.info(this)+" m"+eighthAt+" b"+barAt+" "+tones;
   }
