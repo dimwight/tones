684562dc000a078e9d8130521a1ee5a13af79daa
David Wright <24604124+dimwight@users.noreply.github.com>
2018-05-06 23:03:30 +0100

    Update Incipit.java

--- Tones/src/tones/bar/Incipit.java
+++ Tones/src/tones/bar/Incipit.java
@@ -41,8 +41,8 @@ public final class Incipit extends Tracer implements Comparable<Incipit>{
 			for(Voice v:Voice.values()){
 				Short now=incipitTones.get(v),then=voiceTones.get(v);
 				if(now!=null) nowTones.put(v,now);
-				else if(then!=null) nowTones.put(v,
-						(short)(then-((i.eighthAt>0?i.eighthAt:barEighths)-eighthAt)));
+				else if(then!=null) nowTones.put(v,then.newSounding(i.eighthAt>0?i.eighthAt:barEighths)-eighthAt)
+						(short)(then-(()));
 			}
 			return new Soundings(i.eighthAt,Collections.unmodifiableMap(nowTones));
 		}
