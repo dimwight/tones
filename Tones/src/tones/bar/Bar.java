ed360631a7bbbc442bbae37ccf549508ad82b606
David Wright <24604124+dimwight@users.noreply.github.com>
2018-05-14 14:01:07 +0000

    Update Bar.java

--- Tones/src/tones/bar/Bar.java
+++ Tones/src/tones/bar/Bar.java
@@ -34,7 +34,76 @@ final public class Bar extends Tracer{
     int rise=-1,staveGap=-1,fall=-1;
     for(Incipit i:incipits){
       i.close();
+			List<Voice>toneVoices=new ArrayList();package tones.bar;
+import static java.lang.Math.*;
+import static tones.Tone.*;
+import static tones.Voice.*;
+import static tones.bar.Bar.*;
+import facets.util.Debug;
+import facets.util.Objects;
+import facets.util.Tracer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import tones.Tone;
+import tones.Voice;
+import tones.bar.Incipit.Soundings;
+final public class Bar extends Tracer{
+	public static final int WIDTH_NOTE=8;
+  private static final int WIDTH_SPACE_SHRINK=(false?0:WIDTH_NOTE*2/3),
+    START_AT=Bar.WIDTH_NOTE/2;
+  public final int at,rise,staveGap,fall,width;
+  public final Set<Incipit>incipits;
+  public final Soundings endSoundings;
+  private final Map<Voice,Integer>voiceAts=new HashMap();
+  private int gridAt=false?0:START_AT;
+  Bar(int barAt,List<Incipit>incipits,int barEighths){
+    this.at=barAt;
+    if(incipits==null)throw new IllegalStateException(
+        "Null incipits in "+Debug.info(this));
+    else this.incipits=new HashSet(incipits);
+    for(Voice voice:voiceList)voiceAts.put(voice,START_AT);
+    int rise=-1,staveGap=-1,fall=-1;
+    for(Incipit i:incipits){
+      i.close();
 			List<Voice>toneVoices=new ArrayList();
+			for(Tone t:i.tones)toneVoices.add(t.voice);
+			gridAt=i.barAt=furthestAt(toneVoices);
+			for(Tone t:i.tones)
+				voiceAts.put(t.voice,gridAt+Tone.gridAfter(t.eighths,WIDTH_NOTE));
+      rise=max(rise,i.rise);
+      staveGap=max(staveGap,i.staveGap);
+      fall=max(fall,i.fall);
+			
+    }
+    width=gridAt=furthestAt(voiceList);
+    this.rise=rise;
+    this.staveGap=staveGap;
+    this.fall=fall;
+    endSoundings=incipits.get(incipits.size()-1).soundings();
+    
+  }
+  int furthestAt(Iterable<Voice>voices){
+    int furthest=0;
+    for(Voice voice:voices)
+      furthest=max(furthest,voiceAts.get(voice));
+    return furthest;
+  }
+  @Override
+  public boolean equals(Object obj){
+    Bar that=(Bar)obj;
+    return this==that||incipits.equals(that.incipits);
+  }
+  public String toString(){
+    return Debug.info(this)+" at="+at+" incipits="
+    + (true?incipits.size():"\n"+Objects.toLines(((Collection)incipits).toArray()));
+  }
+}
+
 			for(Tone t:i.tones)toneVoices.add(t.voice);
 			gridAt=i.barAt=furthestAt(toneVoices);
 			for(Tone t:i.tones)
