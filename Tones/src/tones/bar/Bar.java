caf9967fc7ccba78db7a38bb2963ad5c3c527e55
David Wright <24604124+dimwight@users.noreply.github.com>
2018-06-01 20:49:48 +0100

    Update Bar.java

--- Tones/src/tones/bar/Bar.java
+++ Tones/src/tones/bar/Bar.java
@@ -24,7 +24,7 @@ final public class Bar extends Tracer{
 	public final Soundings endSoundings;
 	Bar(int barAt,List<Incipit>incipits,int barEighths){
 		this.at=barAt;
-		final int spread=WIDTH_NOTE*barEighths/incipits.size();
+		final double spread=WIDTH_NOTE*barEighths/incipits.size();
 		if(incipits==null)throw new IllegalStateException(
 				"Null incipits in "+Debug.info(this));
 		else this.incipits=new HashSet(incipits);
