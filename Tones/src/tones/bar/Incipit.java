26d72c7c4aed532302e9a355a72722e87f0f20a2
David Wright <24604124+dimwight@users.noreply.github.com>
2018-06-01 21:41:33 +0100

    Update Incipit.java

--- Tones/src/tones/bar/Incipit.java
+++ Tones/src/tones/bar/Incipit.java
@@ -96,11 +96,13 @@ public final class Incipit extends Tracer implements Comparable<Incipit>{
 		}
 		return set;
 	}
-	void close(){
+	void close(int gridAt,double spread){
+		barAt=gridAt*spread;
 		for(Tone t:tones)t.checkBarOffset(this,Bar.WIDTH_NOTE);
 		rise=6;
 		staveGap=10;
-		fall=6;
+		fall=6; 
+		return Bar.WIDTH_NOTE+maxOffset;
 	}
 	public int hashCode(){
 		return Arrays.hashCode(intValues());
