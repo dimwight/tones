f19a5ef5c66462c44034682e6174043870e78e12
David Wright <24604124+dimwight@users.noreply.github.com>
2018-06-02 17:22:51 +0100

    Update Bar.java

--- Tones/src/tones/bar/Bar.java
+++ Tones/src/tones/bar/Bar.java
@@ -44,9 +44,6 @@ final public class Bar extends Tracer{
 		this.fall=fall;
 		endSoundings=incipits.get(incipits.size()-1).soundings();
 	}
-	static int eighthSpacedGridAt(int gridAt,int eighthAt,double spread){
-		return Math.Max(gridAt,true?0:eighthAt*spread);
-	}
 	@Override
 	public boolean equals(Object obj){
 		Bar that=(Bar)obj;
