9d864760c59cd7fff89279617714fa91eeb22841
David Wright <24604124+dimwight@users.noreply.github.com>
2018-06-02 17:24:53 +0100

    Update Bar.java

--- Tones/src/tones/bar/Bar.java
+++ Tones/src/tones/bar/Bar.java
@@ -28,9 +28,9 @@ final public class Bar extends Tracer{
 				"Null incipits in "+Debug.info(this));
 		else this.incipits=new HashSet(incipits);
 		this.at=barAt;
+		final double spread=SPREAD_BASE*barEighths/incipits.size();
 		BiFunction<int,int,int>eighthSpacedGridAt=
 			(gridAt,eighthAt)=>(int)Math.Max(gridAt,true?0:eighthAt*spread);
-		final double spread=SPREAD_BASE*barEighths/incipits.size();
 		int rise=-1,staveGap=-1,fall=-1,gridAt=START_AT;
 		for(Incipit i:incipits){
 			gridAt=i.close(gridAt,eighthSpacedGridAt);
